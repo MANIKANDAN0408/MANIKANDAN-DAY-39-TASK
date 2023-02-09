a02d501cc1c979540cd5c215bd904c2d0ecad1ec
MANIKANDAN0408 <115692293+MANIKANDAN0408@users.noreply.github.com>
2023-02-09 08:49:11 +0530

    Add files via upload

---
+++ DAY 39 TASK/index.js
@@ -0,0 +1,27 @@
+
+const fs=require('fs')
+const express=require("express");
+
+const router=require('./router/router');
+const dotenv=require('dotenv')
+
+
+dotenv.config();
+const PORT=process.env.PORT || 3000;
+
+const app=express();
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("Please use endpoints: /file/create- to create &&  /file/list- to lists");
+});
+
+
+app.use('/file',router); 
+
+
+app.listen(PORT,()=>{
+    
+    console.log(`App is running on the port: ${PORT}`);
+})
